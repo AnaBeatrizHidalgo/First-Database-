65041a28dab997dfe17d75f75359326bf052d945
Asuka27 <hidalgo.anabeatriz@gmail.com>
2025-05-04 16:30:19 -0300

    chand again

--- Consultas/execute_secondquery.py
+++ Consultas/execute_secondquery.py
@@ -10,15 +10,15 @@ engine = create_engine(DB_URL)
 query = text('''
 SELECT  C."Name" AS Country,
         Y.year,
-        P."Name" AS Energy,
-        P."Renewble" AS Is_Renewble
-        CPS."Power_Generation"
+        PS."Name" AS Energy,
+        PS."Renewable",
+        CPS."Power_Generation",
+        CPS."CO2_Emission" 
 FROM    "Country_Power Source" CPS
 JOIN    "Country"      C  ON CPS."Country_ID_Country" = C."ID_Country"
 JOIN    "Year"         Y  ON Y.year                   = CPS."Year_ID_year"
 JOIN    "Power Source" PS ON CPS."Power Source_ID_Power" = PS."ID_Power"
-GROUP BY C."Name", Y.year
-ORDER BY Y.year DESC, total_twh DESC;
+ORDER BY Y.year DESC, C."Name";
 ''')
 
 with engine.begin() as conn:
